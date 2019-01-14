f33785a6697b8ca1dbc8570a677d87f1b5cf528f
Wilbert Schadee <wilbert_schadee_91@hotmail.com>
2019-01-14 10:49:14 +0100

    redirect login

--- login.php
+++ login.php
@@ -23,11 +23,11 @@ require 'connect.php';
 if(isset($_POST['login'])){
     
     //Retrieve the field values from our login form.
-    $userEmail = !empty($_POST['email']) ? trim($_POST['email']) : null;
+    $userEmail = !empty($_POST['userEmail']) ? trim($_POST['userEmail']) : null;
     $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
 
     //Retrieve the user account information for the given user email.
-    $sql = "SELECT user_email, user_password FROM users WHERE user_email = :user_email";
+    $sql = "SELECT user_id, user_email, user_password FROM users WHERE user_email = :user_email";
     $stmt = $pdo->prepare($sql);
  
     
@@ -50,72 +50,28 @@ if(isset($_POST['login'])){
         //User account found. Check to see if the given password matches the
         //password hash that we stored in our users table.
 
-        
-
         //Compare the passwords.
         $validPassword = password_verify($passwordAttempt, $user['user_password']);
         
-        // echo($user['user_password']);
-        echo($validPassword= password_verify($passwordAttempt, $user['user_password']));
-        // echo($passwordAttempt);
-
         //If $validPassword is TRUE, the login has been successful.
         if($validPassword ){
             
             //Provide the user with a login session.
-            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_id'] = $user['user_id'];
             $_SESSION['logged_in'] = time();
-            $_SESSION['user'] = $user['userEmail'];
+            $_SESSION['user'] = $user['user_email'];
             // $_SESSION['role'] = $user['role'];
-            echo ($_SESSION['user_id']);
             //Redirect to our protected page, which we called home.php
-            // header('Refresh: 1 ; url=home.php');
+            header('Refresh: 1 ; url=user.home.php');
             exit;
             
         } else{
             //$validPassword was FALSE. Passwords do not match.
-            // die
-            echo ('Incorrect username / password combination!, Please try Again');
-            // header ('Refresh: 1 ; url=login.php');
-            
+            // die('Incorrect username / password combination!, Please try Again');
+            header ('Refresh: 1 ; url=loginform.php'); 
         }
     }
     
 }
  
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Login</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-    <body>
-        <div class="form-wrapper">
-            <div class="contact-form">
-            <h1>Login</h1>
-            <form action="login.php" method="post">
-            <label for="useremail">Username</label>
-            <input type="text" id="email" name="email" placeholder="Enter your E-mail"><br>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password"><br>
-            <button type="submit" class="login" name="login" value="Login">Login</button>
-            </form>
-            </div>
-        
-            
-        </div>          
-    </body>
-</html>
-
-
-
-
-
-
-
-
-
-
---
+++ loginform.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div class="form-wrapper">
+            <div class="contact-form">
+            <h1>Login</h1>
+            <form action="login.php" method="post">
+            <label for="useremail">Username</label>
+            <input type="text" id="userEmail" name="userEmail" placeholder="Enter your E-mail"><br>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password"><br>
+            <button type="submit" class="login" name="login" value="Login">Login</button>
+            </form>
+            </div>  
+        </div>          
+    </body>
+</html>
